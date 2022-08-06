67bfd614ff29cfc984721662543be48aa2f716ed
Oliver Kühne <olli.kuehne@gmail.com>
2022-08-06 15:32:54 +0200

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alexedwards/scs/v2"
+	"github.com/okuehne/bookings/pkg/config"
+	"github.com/okuehne/bookings/pkg/handlers"
+	"github.com/okuehne/bookings/pkg/render"
+	"log"
+	"net/http"
+	"time"
+)
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+const portNumber = ":8080"
+
+// main is the main application function
+func main() {
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
